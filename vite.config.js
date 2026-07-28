cc2d70bce73b855c3ad142bae528e1f40ebbfe90
phakin558 <pakincharatsri@gmail.com>
2026-07-28 22:48:18 +0700

    Fourth

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/MY-KMITL/',
+  base: '/Phakin-Calendar/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
