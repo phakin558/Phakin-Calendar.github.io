d75637e27d9e5a62db3a3d4dd35164f7f9b62592
phakin558 <pakincharatsri@gmail.com>
2026-07-28 21:33:20 +0700

    Setup project with gitignore

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
+  base: './',   // 🟢 เพิ่มบรรทัดนี้! ให้อ้างไฟล์แบบ relative ใช้ได้ทั้ง Vercel และ GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      // 🟢 บรรทัดนี้สำคัญมาก! บังคับให้ Vite ดึงตัว Vue Build ที่มี Template Compiler ไปใช้ตอน Deploy
-      'vue': 'vue/dist/vue.esm-bundler.js', 
+      'vue': 'vue/dist/vue.esm-bundler.js',
     },
   },
-  // 🟢 เพิ่มส่วนนี้เพื่ออนุญาตให้สแกน Vue Template ในไฟล์ JS ได้
   define: {
     __VUE_OPTIONS_API__: true,
     __VUE_PROD_DEVTOOLS__: false,
