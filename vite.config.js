c53402c723642be1f1f9c2673f30c7f33b504885
phakin558 <pakincharatsri@gmail.com>
2026-07-29 22:30:02 +0700

    delete dot github

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Phakin-Calendar.github.io/',
+  base: '/Phakin-Calendar/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
